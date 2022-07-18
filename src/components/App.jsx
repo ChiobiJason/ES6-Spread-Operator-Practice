b17be4c35b3ea25393104624940e7f24ff284949
ChiobiJason <chiobichisom@gmail.com>
2022-07-18 08:28:19 +0000

    Finished Challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setInputText(value);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText]
+    })
+    setInputText("");
+  }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} value={inputText} type="text" />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((toDoItem) => <li>{toDoItem}</li>)}
         </ul>
       </div>
     </div>
